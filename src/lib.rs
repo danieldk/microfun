c0e798963076ab0df49b7874eec09b22b3d8a77d
Daniël de Kok <me@danieldk.eu>
2019-01-08 20:10:30 +0100

    First stab at reading the temperature

---
+++ src/lib.rs
@@ -0,0 +1,16 @@
+#![no_std]
+
+use microbit::TEMP;
+
+pub struct Temperature(pub TEMP);
+
+impl Temperature {
+    pub fn temperature(&self) -> u32 {
+        self.0.tasks_start.write(|w| unsafe { w.bits(1) });
+        while self.0.events_datardy.read().bits() == 0 {}
+        self.0.events_datardy.reset();
+        let t = self.0.temp.read().bits() / 4;
+        self.0.tasks_stop.write(|w| unsafe { w.bits(1) });
+        t
+    }
+}

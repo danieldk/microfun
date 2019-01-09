0f0ff529199bf3b739fd8edae51064ab4bb700a6
Daniël de Kok <me@danieldk.eu>
2019-01-09 20:36:45 +0100

    temperature: use scrolling text

--- src/bin/temperature.rs
+++ src/bin/temperature.rs
@@ -8,8 +8,11 @@ use microbit::hal::delay::Delay;
 use microbit::hal::prelude::*;
 use microbit::hal::serial;
 use microbit::hal::serial::BAUD115200;
+use microbit::led;
 use panic_halt;
 
+use microfun::display::ScrollImages;
+use microfun::font::Font;
 use microfun::Temperature;
 
 #[entry]
@@ -22,8 +25,48 @@ fn main() -> ! {
         let (mut tx, _) = serial::Serial::uart0(p.UART0, tx, rx, BAUD115200).split();
         let mut delay = Delay::new(p.TIMER0);
 
+        // Display
+        let row1 = gpio.pin13.into_push_pull_output();
+        let row2 = gpio.pin14.into_push_pull_output();
+        let row3 = gpio.pin15.into_push_pull_output();
+        let col1 = gpio.pin4.into_push_pull_output();
+        let col2 = gpio.pin5.into_push_pull_output();
+        let col3 = gpio.pin6.into_push_pull_output();
+        let col4 = gpio.pin7.into_push_pull_output();
+        let col5 = gpio.pin8.into_push_pull_output();
+        let col6 = gpio.pin9.into_push_pull_output();
+        let col7 = gpio.pin10.into_push_pull_output();
+        let col8 = gpio.pin11.into_push_pull_output();
+        let col9 = gpio.pin12.into_push_pull_output();
+        let mut leds = led::Display::new(
+            col1, col2, col3, col4, col5, col6, col7, col8, col9, row1, row2, row3,
+        );
+
+        let font = Font::default();
+
+        let digit3 = font.char_bits(b'C');
+
         loop {
-            write!(tx, "Temperature: {}\n\r", temp.temperature()).unwrap();
+            let current_temp = temp.temperature();
+            write!(tx, "Temperature: {}\n\r", current_temp).unwrap();
+
+            let digit1 = font.char_bits((current_temp / 10) as u8 + 48);
+            let digit2 = font.char_bits((current_temp % 10) as u8 + 48);
+
+            let mut digits = [[0; 5]; 20];
+            let digit0 = if current_temp < 0 {
+                font.char_bits(b'-')
+            } else {
+                font.char_bits(b'+')
+            };
+
+            digits[0..5].copy_from_slice(&digit0);
+            digits[5..10].copy_from_slice(&digit1);
+            digits[10..15].copy_from_slice(&digit2);
+            digits[15..].copy_from_slice(&digit3);
+
+            leds.scroll_images(&mut delay, &digits, 200);
+
             delay.delay_ms(1000_u32);
         }
     }
